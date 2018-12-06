8a9c7df3eb3c7ab2efa9b92b94ce8d14ca6828b4
AlexW <429774648@qq.com>
2018-12-07 00:43:24 +0800

    seg_cycle

--- my_seg_depth/train.py
+++ my_seg_depth/train.py
@@ -4,7 +4,7 @@ from datasets.dataset_synthia import dataloader
 from util.visualizer import Visualizer
 from tensorboardX import SummaryWriter
 import torch
-#from models import
+#from .model import create_model_segdepth
 from  .networks import G_1,Discriminator,Feature_net,SEG,DEP
 
 if __name__ == '__main__':
@@ -20,12 +20,13 @@ if __name__ == '__main__':
 
     dataset_train = dataloader(opt, train_or_test='train')
     dataset_test = dataloader(opt, train_or_test='test')
-    G_syn = G_1(input_nc=3, out_nc=128).cuda()
-    G_real = G_1(input_nc=3, out_nc=128).cuda()
-    Dis = Discriminator().cuda()
-    F = Feature_net().cuda()
-    Seg_net = SEG().cuda()
-    Dep_net = DEP().cuda()
+    model = create_model_segdepth(opt)
+    #G_syn = G_1(input_nc=3, out_nc=128).cuda()
+    #G_real = G_1(input_nc=3, out_nc=128).cuda()
+    #Dis = Discriminator().cuda()
+    #F = Feature_net().cuda()
+    #Seg_net = SEG().cuda()
+    #Dep_net = DEP().cuda()
     visualizer = Visualizer(opt)
     total_steps = 0
     global_iter=0
@@ -54,6 +55,43 @@ if __name__ == '__main__':
                         im = torch.from_numpy(img.transpose([2, 0, 1])).squeeze(0)
                         writer_test.add_image("{}test/img_{}".format(opt.name, name), im, global_iter + ii)
                 print("validation done")
+
+                global_iter += 1
+                iter_start_time = time.time()
+                if total_steps % opt.print_freq == 0:
+                    t_data = iter_start_time - iter_data_time
+                visualizer.reset()
+                total_steps += opt.batch_size
+                epoch_iter += opt.batch_size
+
+                model.set_input(data)
+                model.optimize_parameters(train_or_test='train')
+                if (global_iter % 50 == 0):
+                    errors = model.get_current_losses()
+                    for name, error in errors.items():
+                        print('------------------')
+                        writer_train.add_scalar("{}train/{}".format(opt.name, name), error, global_iter)
+                    images = model.get_current_visuals()
+
+                    for name, img in images.items():
+                        img = img / img.max()
+                        img = torch.from_numpy(img.transpose([2, 0, 1]))
+                        writer_train.add_image("{}train/img_{}".format(opt.name, name), img, global_iter)
+                if total_steps % opt.save_latest_freq == 0:
+                    print('saving the latest model (epoch %d, total_steps %d)' %
+                          (epoch, total_steps))
+                    model.save_networks('iter_%d' % total_steps)
+
+                iter_data_time = time.time()
+
+            if epoch % 5 == 0:
+                print('saving the model at the end of epoch %d, iters %d' %
+                      (epoch, total_steps))
+                model.save_networks(epoch)
+
+            print('End of epoch %d / %d \t Time Taken: %d sec' %
+                  (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+            model.update_learning_rate()
 
 
 
