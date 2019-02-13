fa12e075444af00eb58065a7622b4fdd29cdcae4
AlexW <429774648@qq.com>
2019-02-13 21:58:56 +0800

    seg_cycle

--- new_model/my_rf_lw.py
+++ new_model/my_rf_lw.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
-from new_model.utils.layer_factory import conv1x1, conv3x3, CRPBlock,PSPBlock,ATBlock
+from new_model.utils.layer_factory import convbnrelu,conv1x1, conv3x3, CRPBlock,PSPBlock,ATBlock
 
 data_info = {
     7 : 'Person',
@@ -130,6 +130,42 @@ class Bottleneck(nn.Module):
         return out
 
 
+
+class DCGAN_D(torch.nn.Module):
+	def __init__(self,D_h_size=64):
+		super(DCGAN_D, self).__init__()
+		main = torch.nn.Sequential()
+
+		### Start block
+		# Size = n_colors x image_size x image_size
+		main.add_module('Start-Conv2d', torch.nn.Conv2d(2, D_h_size, kernel_size=4, stride=2, padding=1, bias=False))
+		main.add_module('Start-SELU', torch.nn.SELU(inplace=True))
+
+		#image_size_new = param.image_size // 2
+		# Size = D_h_size x image_size/2 x image_size/2
+
+		### Middle block (Done until we reach ? x 4 x 4)
+		mult = 1
+		i = 0
+		for i in range(5):
+			main.add_module('Middle-Conv2d [%d]' % i, torch.nn.Conv2d(D_h_size * mult, D_h_size * (2*mult), kernel_size=4, stride=2, padding=1, bias=False))
+			main.add_module('Middle-SELU [%d]' % i, torch.nn.SELU(inplace=True))
+
+			mult *= 2
+			#i += 1
+
+		### End block
+		# Size = (D_h_size * mult) x 4 x 4
+		main.add_module('End-Conv2d', torch.nn.Conv2d(D_h_size * mult, 1, kernel_size=4, stride=1, padding=0, bias=False))
+		main.add_module('End-Sigmoid', torch.nn.Sigmoid())
+		# Size = 1 x 1 x 1 (Is a real cat or not?)
+		self.main = main
+
+	def forward(self, input):
+		output = torch.nn.parallel.data_parallel(self.main, input, range(2))
+
+		return output.view(-1)
+
 class depth_block(nn.Module):
     def __init__(self,in_c):
         super(depth_block, self).__init__()
@@ -139,24 +175,23 @@ class depth_block(nn.Module):
         for i in range(4):
             self.upconv.append(nn.Sequential(nn.ConvTranspose2d(in_c,int(in_c/2),4,2,padding=1) , nn.LeakyReLU(0.02)
             ,nn.BatchNorm2d(int(in_c/2)),
-            nn.Conv2d(int(in_c / 2), int(in_c / 2), 3, 1, padding=1),nn.LeakyReLU(0.02),nn.BatchNorm2d(int(in_c/2))
+            nn.Conv2d(int(in_c / 2), int(in_c / 2), 1, 1),nn.ReLU6(),nn.BatchNorm2d(int(in_c/2))
                                              ))
 
             self.depth_out.append(nn.Sequential(nn.Conv2d(int(in_c/2),1,3,1,padding=1),nn.Tanh()
                                                 ))
 
-            self.attention_bs.append(nn.Sequential(nn.Conv2d(in_c,int(in_c/2),3,2,padding=1) , nn.LeakyReLU(0.02)
+            self.attention_bs.append(nn.Sequential(nn.Conv2d(in_c,int(in_c/2),3,2,padding=1) , nn.ReLU6()
             ,nn.BatchNorm2d(int(in_c/2)),
 
-            nn.Conv2d(int(in_c / 2), int(in_c / 2), 3, 2, padding=1),nn.LeakyReLU(0.02)
-            ,nn.AdaptiveAvgPool2d(1)))
+            nn.AdaptiveAvgPool2d(1)))
         #self.depth_out.append(nn.Sequential(nn.Conv2d(int(in_c / 2), 1, 1, 1), nn.BatchNorm2d(1), nn.Tanh()))
 
         self.at_act = nn.Sigmoid()
-        self.conv = nn.Sequential(nn.Conv2d(int(in_c*2),int(in_c),3,1,padding=1),
+        self.conv = nn.Sequential(nn.Conv2d(int(in_c*2),int(in_c/2),3,1,padding=1),
                                      nn.LeakyReLU(0.02),
-                                  nn.BatchNorm2d(in_c),
-                                  nn.Conv2d(int(in_c), int(in_c / 2), 3, 1, padding=1), nn.LeakyReLU(0.02),nn.BatchNorm2d(int(in_c / 2))
+                                  nn.BatchNorm2d(in_c//2),
+
                                                  )
 
 
@@ -164,8 +199,8 @@ class depth_block(nn.Module):
         #self.s_econv = nn.Sequential(
          #                            nn.Conv2d(int(in_c/2),1,3,1,padding=1),nn.BatchNorm2d(1),
           #                           nn.Sigmoid())
-        self.depconv = nn.Sequential(nn.Conv2d(int(in_c/2),1,3,stride=1,padding=1),nn.BatchNorm2d(1),
-                                     #nn.Tanh()
+        self.depconv = nn.Sequential(nn.Conv2d(int(in_c/2),1,3,stride=1,padding=1),#nn.BatchNorm2d(1),
+                                     nn.Tanh()
                                      )
 
     def forward(self,in_f):
@@ -200,22 +235,35 @@ class ResNetLW(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
+        self.ins_layer1_s = self._make_instancelayer(planes=256)
+        self.ins_layer1_r = self._make_instancelayer(planes=256)
+
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.ins_layer2_s = self._make_instancelayer(planes=512)
+        self.ins_layer2_r = self._make_instancelayer(planes=512)
+
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.ins_layer3_s = self._make_instancelayer(planes=1024)
+        self.ins_layer3_r = self._make_instancelayer(planes=1024)
+
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.p_ims1d2_outl1_dimred = conv1x1(2048, 512, bias=False)
+        self.ins_layer4_s = self._make_instancelayer(planes=2048)
+        self.ins_layer4_r = self._make_instancelayer(planes=2048)
+
+
+        self.p_ims1d2_outl1_dimred_ = convbnrelu(2048,1024,1)
        # self.mflow_conv_g1_pool = self._make_crp(512, 512, 4)
-        self.psp4 = self._make_psp(512, 512)
-        self.mflow_conv_g1_b3_joint_varout_dimred = conv1x1(512, 256, bias=False)
-        self.p_ims1d2_outl2_dimred = conv1x1(1024, 256, bias=False)
-        self.adapt_stage2_b2_joint_varout_dimred = conv1x1(256, 256, bias=False)
+        self.psp4 = self._make_psp(1024, 1024)
+        self.mflow_conv_g1_b3_joint_varout_dimred = conv1x1(1024,512, bias=False)
+        self.p_ims1d2_outl2_dimred_= convbnrelu(1024, 512, 1)
+        self.adapt_stage2_b2_joint_varout_dimred = conv1x1(512, 512, bias=False)
        # self.mflow_conv_g2_pool = self._make_crp(256, 256, 4)
-        self.psp3 = self._make_psp(256,256)
-        self.CAT3 = ATBlock(256,256)
+        self.psp3 = self._make_psp(512,512)
+        self.CAT3 = ATBlock(512,512)
 
-        self.mflow_conv_g2_b3_joint_varout_dimred = conv1x1(256, 256, bias=False)
+        self.mflow_conv_g2_b3_joint_varout_dimred = conv1x1(512, 256, bias=False)
 
-        self.p_ims1d2_outl3_dimred = conv1x1(512, 256, bias=False)
+        self.p_ims1d2_outl3_dimred_ = convbnrelu(512, 256,1)
         self.adapt_stage3_b2_joint_varout_dimred = conv1x1(256, 256, bias=False)
         #self.mflow_conv_g3_pool = self._make_crp(256, 256, 4)
         self.psp2 = self._make_psp(256, 256)
@@ -223,7 +271,7 @@ class ResNetLW(nn.Module):
 
         self.mflow_conv_g3_b3_joint_varout_dimred = conv1x1(256, 256, bias=False)
 
-        self.p_ims1d2_outl4_dimred = conv1x1(256, 256, bias=False)
+        self.p_ims1d2_outl4_dimred_ = convbnrelu(256, 256,1)
         self.adapt_stage4_b2_joint_varout_dimred = conv1x1(256, 256, bias=False)
        # self.mflow_conv_g4_pool = self._make_crp(256, 256, 4)
         self.psp1 = self._make_psp(256, 256)
@@ -261,70 +309,152 @@ class ResNetLW(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-       # print('1',x.shape)
+    def _make_instancelayer(self, planes, stride=1):
+        ins_layer = nn.Sequential(
+                nn.Conv2d(planes, planes,
+                          kernel_size=1, stride=1, bias=False),
+                nn.InstanceNorm2d(planes),
+               # nn.Conv2d(planes, planes,
+                #      kernel_size=1, stride=1, bias=False),
+               # nn.InstanceNorm2d(planes)
+         )
+        return ins_layer
 
-        l1 = self.layer1(x)
-       # print('2',l1.shape)
-        l2 = self.layer2(l1)
-       # print('3',l2.shape)
-        l3 = self.layer3(l2)
-       # print('4', l3.shape)
-        l4 = self.layer4(l3)
-       # print('5', l4.shape)
+    def forward(self, x,type='real'):
+        if type=='real':
+            x = self.conv1(x)
+            x = self.bn1(x)
+            x = self.relu(x)
+            x = self.maxpool(x)
 
-      #  l4 = self.do(l4)
-      #  l3 = self.do(l3)
-        #print(l3.shape)
+            l1 = self.layer1(x)
+            l1 = self.ins_layer1_r(l1)
+           # print('2',l1.shape)
+            l2 = self.layer2(l1)
+            l2 = self.ins_layer2_r(l2)
+           # print('3',l2.shape)
+            l3 = self.layer3(l2)
+            l3 = self.ins_layer3_r(l3)
+           # print('4', l3.shape)
+            l4 = self.layer4(l3)
+            l4 = self.ins_layer4_r(l4)
+           # print('5', l4.shape)
 
-        x4 = self.p_ims1d2_outl1_dimred(l4)
-        x4 = self.relu(x4)
-    #    print(x4.shape)
-        #x4 = self.mflow_conv_g1_pool(x4)#crp
-        x4 = self.psp4(x4)
-        x4 = self.mflow_conv_g1_b3_joint_varout_dimred(x4)#rcu*3
-        #print(x4.shape)
-        x4 = nn.Upsample(size=l3.size()[2:], mode='bilinear', align_corners=True)(x4)
-      #  print('de_1',x4.shape)
+          #  l4 = self.do(l4)
+          #  l3 = self.do(l3)
+            #print(l3.shape)
 
-        #print(l3.shape)
-        x3 = self.p_ims1d2_outl2_dimred(l3)
-        x3 = self.adapt_stage2_b2_joint_varout_dimred(x3)
-        x3 = self.CAT3(x3,x4)
+            x4 = self.p_ims1d2_outl1_dimred_(l4)
+            x4 = self.relu(x4)
+        #    print(x4.shape)
+            #x4 = self.mflow_conv_g1_pool(x4)#crp
+            x4 = self.psp4(x4)
+            x4 = self.mflow_conv_g1_b3_joint_varout_dimred(x4)#rcu*3
+            #print(x4.shape)
+            x4 = nn.Upsample(size=l3.size()[2:], mode='bilinear', align_corners=True)(x4)
+          #  print('de_1',x4.shape)
+
+            #print(l3.shape)
+            x3 = self.p_ims1d2_outl2_dimred_(l3)
+            x3 = self.adapt_stage2_b2_joint_varout_dimred(x3)
+            x3 = self.CAT3(x3,x4)
 
 
-        #x3 = F.relu(x3)
-        x3 = self.psp3(x3)
-       # x3 = self.mflow_conv_g2_pool(x3)
+            #x3 = F.relu(x3)
+            x3 = self.psp3(x3)
+           # x3 = self.mflow_conv_g2_pool(x3)
 
 
-        x3 = self.mflow_conv_g2_b3_joint_varout_dimred(x3)
-        x3 = nn.Upsample(size=l2.size()[2:], mode='bilinear', align_corners=True)(x3)
-     #   print('de_2', x3.shape)
+            x3 = self.mflow_conv_g2_b3_joint_varout_dimred(x3)
+            x3 = nn.Upsample(size=l2.size()[2:], mode='bilinear', align_corners=True)(x3)
+         #   print('de_2', x3.shape)
 
-        x2 = self.p_ims1d2_outl3_dimred(l2)
-        x2 = self.adapt_stage3_b2_joint_varout_dimred(x2)
-        x2 = self.CAT2(x2,x3)
-        x2 = self.psp2(x2)
-        x2 = self.mflow_conv_g3_b3_joint_varout_dimred(x2)
-        x2 = nn.Upsample(size=l1.size()[2:], mode='bilinear', align_corners=True)(x2)
-    #    print('de_3', x2.shape)
+            x2 = self.p_ims1d2_outl3_dimred_(l2)
+            x2 = self.adapt_stage3_b2_joint_varout_dimred(x2)
+            x2 = self.CAT2(x2,x3)
+            x2 = self.psp2(x2)
+            x2 = self.mflow_conv_g3_b3_joint_varout_dimred(x2)
+            x2 = nn.Upsample(size=l1.size()[2:], mode='bilinear', align_corners=True)(x2)
+        #    print('de_3', x2.shape)
 
-        x1 = self.p_ims1d2_outl4_dimred(l1)
-        x1 = self.adapt_stage4_b2_joint_varout_dimred(x1)
-     #   x1 = x1 + x2
-        x1 = self.CAT1(x1,x2)
-        x1 = self.psp1(x1)
-    #    x1 = self.mflow_conv_g4_pool(x1)
-   #     print('de_4', x1.shape)
+            x1 = self.p_ims1d2_outl4_dimred_(l1)
+            x1 = self.adapt_stage4_b2_joint_varout_dimred(x1)
+         #   x1 = x1 + x2
+            x1 = self.CAT1(x1,x2)
+            x1 = self.psp1(x1)
+        #    x1 = self.mflow_conv_g4_pool(x1)
+       #     print('de_4', x1.shape)
 
-        outs,pred_d = self.dep(x1)
+            outs,pred_d = self.dep(x1)
 
-        return outs,pred_d,(x4,x3,x2,x1)
+            return outs,pred_d,(x4,x3,x2,x1)
+        else:
+            x = self.conv1(x)
+            x = self.bn1(x)
+            x = self.relu(x)
+            x = self.maxpool(x)
+            # print('1',x.shape)
+
+            l1 = self.layer1(x)
+            l1 = self.ins_layer1_s(l1)
+            # print('2',l1.shape)
+            l2 = self.layer2(l1)
+            l2 = self.ins_layer2_s(l2)
+            # print('3',l2.shape)
+            l3 = self.layer3(l2)
+            l3 = self.ins_layer3_s(l3)
+            # print('4', l3.shape)
+            l4 = self.layer4(l3)
+            l4 = self.ins_layer4_s(l4)
+            # print('5', l4.shape)
+
+            #  l4 = self.do(l4)
+            #  l3 = self.do(l3)
+            # print(l3.shape)
+
+            x4 = self.p_ims1d2_outl1_dimred_(l4)
+            x4 = self.relu(x4)
+            #    print(x4.shape)
+            # x4 = self.mflow_conv_g1_pool(x4)#crp
+            x4 = self.psp4(x4)
+            x4 = self.mflow_conv_g1_b3_joint_varout_dimred(x4)  # rcu*3
+            # print(x4.shape)
+            x4 = nn.Upsample(size=l3.size()[2:], mode='bilinear', align_corners=True)(x4)
+            #  print('de_1',x4.shape)
+
+            # print(l3.shape)
+            x3 = self.p_ims1d2_outl2_dimred_(l3)
+            x3 = self.adapt_stage2_b2_joint_varout_dimred(x3)
+            x3 = self.CAT3(x3, x4)
+
+            # x3 = F.relu(x3)
+            x3 = self.psp3(x3)
+            # x3 = self.mflow_conv_g2_pool(x3)
+
+
+            x3 = self.mflow_conv_g2_b3_joint_varout_dimred(x3)
+            x3 = nn.Upsample(size=l2.size()[2:], mode='bilinear', align_corners=True)(x3)
+            #   print('de_2', x3.shape)
+
+            x2 = self.p_ims1d2_outl3_dimred_(l2)
+            x2 = self.adapt_stage3_b2_joint_varout_dimred(x2)
+            x2 = self.CAT2(x2, x3)
+            x2 = self.psp2(x2)
+            x2 = self.mflow_conv_g3_b3_joint_varout_dimred(x2)
+            x2 = nn.Upsample(size=l1.size()[2:], mode='bilinear', align_corners=True)(x2)
+            #    print('de_3', x2.shape)
+
+            x1 = self.p_ims1d2_outl4_dimred_(l1)
+            x1 = self.adapt_stage4_b2_joint_varout_dimred(x1)
+            #   x1 = x1 + x2
+            x1 = self.CAT1(x1, x2)
+            x1 = self.psp1(x1)
+            #    x1 = self.mflow_conv_g4_pool(x1)
+            #     print('de_4', x1.shape)
+
+            outs, pred_d = self.dep(x1)
+
+            return outs, pred_d, (x4, x3, x2, x1)
 
 def init_weights(net, init_type='normal', gain=0.02):
     def init_func(m):
@@ -360,12 +490,11 @@ def init_net(net, init_type='kaiming', init_gain=0.02):
     init_weights(net, init_type, gain=init_gain)
 
     return net
-
 class segd(nn.Module):
-    def __init__(self,n_cls,up_scale):
+    def __init__(self,n_cls,up_scale,init_channel=256):
         super(segd, self).__init__()
         self.Up = nn.ModuleList()
-        init_channel=256
+        init_channel=init_channel
         for i in range(up_scale):
             self.Up.append(DeconvBlock(init_channel, int(init_channel/2)))
             init_channel=int(init_channel/2)
@@ -417,7 +546,7 @@ if __name__ == '__main__':
     G_dict = net.state_dict()
 
     ori_dict = torch.load('/home/dut-ai/Documents/light-weight-refinenet-master/models/rf_lw101_img.pth')
-    segd8 = segd(n_cls=40,up_scale=3).cuda()
+    segd8 = segd(n_cls=40,up_scale=3,init_channel=512).cuda()
     segd4 = segd(n_cls=40,up_scale=2).cuda()
     segd2 = segd(n_cls=40,up_scale=1).cuda()
     segd2_0 = segd(n_cls=40,up_scale=1).cuda()
@@ -429,14 +558,16 @@ if __name__ == '__main__':
 
 
         if name in G_dict:
-            print(name)
+
             #print(name)
-            G_dict[name] = ori_dict[name]
-            n += 1
+            if G_dict[name].shape == ori_dict[name].shape:
+                print(name)
+                G_dict[name] = ori_dict[name]
+                n += 1
 
         m += 1
     print(n,m)
-    torch.save(G_dict, './my_res101_model.pth')
+    torch.save(G_dict, './my_ins_res101_model.pth')
     print(net)
     outs,pred_d,features = net(x)
     dis1 = segd8(features[0])
